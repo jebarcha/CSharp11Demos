06394f59cf187381d8a01e47f17034963dae5bf9
jebarcha <jebarcha@hotmail.com>
2024-08-26 12:01:21 -0600

    MethodGroup move to a separate class

---
+++ ImprovedMethodGroup/MethodGroupDemo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImprovedMethodGroup;
+public class MethodGroupDemo
+{
+    private static readonly List<int> Ages = Enumerable.Range(0, 100).ToList();
+
+    static int Sum()
+    {
+        return Ages.Where(a => Filter(a)).Sum();
+    }
+
+    int SumMethodGroup()
+    {
+        return Ages.Where(Filter).Sum();
+    }
+
+    static bool Filter(int age)
+    {
+        return age > 50;
+    }
+}
--- ImprovedMethodGroup/Program.cs
+++ ImprovedMethodGroup/Program.cs
@@ -1,16 +1,1 @@
-﻿static readonly List<int> Ages = Enumerable.Range(0, 100).ToList();
-
-int Sum()
-{
-    return Ages.Where(a => Filter(a)).Sum();
-}
-
-int SumMethodGroup()
-{
-    return Ages.Where(Filter).Sum();
-}
-
-static bool Filter(int age)
-{
-    return age > 50;
-}
+﻿
